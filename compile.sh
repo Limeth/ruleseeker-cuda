28964ffe8f1d5b37ce353f55e34d3b6498941307
Jakub Hlusička <jakub.hlusicka@email.cz>
2021-11-30 22:51:59 +0100

    Move C/C++/CUDA/shader source files to a new `src` directory

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 ./include-shaders.sh
-nvcc *.cu -lGLEW -lglut -lGL -lpng -lbsd -o build-release -O3 -DNDEBUG -DPNG_NO_SETJMP $@
+nvcc src/*.cu -lGLEW -lglut -lGL -lpng -lbsd -o build-release -O3 -DNDEBUG -DPNG_NO_SETJMP $@
--- include-shaders.sh
+++ include-shaders.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # This script converts the shaders
+pushd src
 HEADER_FILE="shaders.cuh"
 rm -f $HEADER_FILE
 
@@ -8,3 +9,4 @@ echo "#pragma once" >>$HEADER_FILE
 for SHADER_FILE in `ls config.h config_derived.h *.vert *.frag`; do
     xxd -i $SHADER_FILE >>$HEADER_FILE
 done
+popd
