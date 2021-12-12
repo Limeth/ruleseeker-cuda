3f4f85e1aa3bfbae646e72e81afff399f0890613
Jakub Hlusička <jakub.hlusicka@email.cz>
2021-12-12 18:03:32 +0100

    Improve config

--- src/config.h
+++ src/config.h
@@ -32,15 +32,15 @@
 
 // # Grid Geometry
 
-// [uint] simulation grid width
+// [uint] Simulation grid width.
 #define GRID_WIDTH  32
-// [uint] simulation grid height
+// [uint] Simulation grid height.
 #define GRID_HEIGHT 32
-// [enum] the shape of the grid's cells (square for Conway's GoL)
+// [enum] The shape of the grid's cells (square for Conway's GoL).
 #define GRID_GEOMETRY GRID_GEOMETRY_HEXAGON
-// [enum] which cells are considered in the neighbourhood (vertex for Conway's GoL)
+// [enum] Which cells are considered in the neighbourhood (vertex for Conway's GoL).
 #define CELL_NEIGHBOURHOOD_TYPE CELL_NEIGHBOURHOOD_TYPE_EDGE
-// [uchar] number of states a cell can become (2 for Conway's GoL)
+// [uchar] Number of states a cell can become (2 for Conway's GoL).
 #define CELL_STATES 2
 
 // Conway's GoL:
@@ -48,74 +48,81 @@
 /* #define CELL_NEIGHBOURHOOD_TYPE CELL_NEIGHBOURHOOD_TYPE_VERTEX */
 /* #define CELL_STATES 2 */
 
-// # Seeking parameters (genetic algorithm)
-// Number of cellular automata to simulate simultaneously.
-#define POPULATION_SIZE 64
-// Number of top candidates to keep for the next population, without any adjustments.
-#define POPULATION_ELITES 2
-// Number of top candidates to use for crossover and mutation (including elites).
-#define POPULATION_SELECTION 16
-// The chance that a single rule of a ruleset changes during mutation.
-#define MUTATION_CHANCE 0.5
-// Whether to make mutation chance dependent on rank -- lower mutation chance for high fitness candidates.
-#define MUTATION_CHANCE_ADAPTIVE_BY_RANK true
 
+// # Seeking parameters (genetic algorithm)
+
+// [uint] Number of cellular automata to simulate simultaneously.
+#define POPULATION_SIZE 64
+// [uint] Number of top candidates to keep for the next population, without any adjustments.
+#define POPULATION_ELITES 2
+// [uint] Number of top candidates to use for crossover and mutation (including elites).
+#define POPULATION_SELECTION 16
+// [float] The chance that a single rule of a ruleset changes during mutation.
+#define MUTATION_CHANCE 0.5
+// [bool] Whether to make mutation chance dependent on rank -- lower mutation chance for high fitness candidates.
+#define MUTATION_CHANCE_ADAPTIVE_BY_RANK true
+// [enum] The method used for the crossover operation.
 #define CROSSOVER_METHOD CROSSOVER_METHOD_SPLICES(64)
-/* #define CROSSOVER_METHOD CROSSOVER_METHOD_UNIFORM */
+// [enum] The method used for the mutation operation.
 #define MUTATION_METHOD MUTATION_METHOD_BINOMIAL_KERNEL
-/* #define MUTATION_METHOD MUTATION_METHOD_UNIFORM */
 
 // ## Fitness function
+
+// [uint; N] N iteration indices at which to evaluate the fitness function.
 #define FITNESS_FN_TARGET_ITERATIONS 50, 100, 150, 200, 201, 202, 203
+// [uint; N] N target fit cell proportions evaluated at corresponding FITNESS_FN_TARGET_ITERATIONS indices.
 #define FITNESS_FN_TARGET_VALUES 0.55, 0.05, 0.55, 0.0, 0.0, 0.0, 0.0
 
-// Proportion
-// [enum] which cell evaluation method to use (decide whether a given cell is "fit", or not)
+// ### Proportion
+
+// [enum] Which cell evaluation method to use (decide whether a given cell is "fit", or not).
 #define FITNESS_EVAL FITNESS_EVAL_UPDATE
-// [uint] which state to count the proportion of
+// [uint] Which state to count the proportion of.
 #define FITNESS_EVAL_STATE_INDEX 0
-// [enum] which way to evaluate the fitness function
+// [enum] Which way to evaluate the fitness function.
 #define FITNESS_FN_TYPE FITNESS_FN_TYPE_LIKELIHOOD
-// [float] what is the target value of the fitness function (the value at which there is a maximum)
-#define FITNESS_FN_TARGET 0.95f
+
 
 // # Miscellaneous
 
-// [uint] execution block width and height
+// [uint] Execution block width and height.
 #define BLOCK_LENGTH 16
-// [bool] whether to verify the GPU simulation with an equivalent CPU simulation
+// [bool] Whether to verify the GPU simulation with an equivalent CPU simulation.
 #define CPU_VERIFY false
-// [bool] `true` to keep aspect ratio, `false` to stretch to window
+// [bool] `true` to keep aspect ratio, `false` to stretch to window.
 #define KEEP_ASPECT_RATIO true
-// [bool] `true` if multisampling should be enabled to fix "jagged" edges, `false` otherwise
+// [bool] `true` if multisampling should be enabled to fix "jagged" edges, `false` otherwise.
 #define USE_MULTISAMPLING true
-// [uint] number of samples per pixel to use when multisampling is enabled
+// [uint] Number of samples per pixel to use when multisampling is enabled.
 #define MULTISAMPLING_SAMPLES 16
-// [bool] whether to display 2-state simulations using more colors, based on the cell action
+// [bool] Whether to display 2-state simulations using more colors, based on the cell action.
 #define MULTICOLOR_TWO_STATE false
-// [float/uint] number of iterations per second
+// [float/uint] Number of iterations per second.
 #define FRAMERATE 8
-// [uint] number of milliseconds to wait between iterations (overriden by FRAMERATE)
+// [uint] Number of milliseconds to wait between iterations (overriden by FRAMERATE).
 /* #define SLEEP_MS 5000 */
-// [uint] initial window width
+// [uint] Initial window width.
 #define WINDOW_WIDTH 1000
-// [uint] initial window height
+// [uint] Initial window height.
 #define WINDOW_HEIGHT 1000
-// [uint] max number of frames exported as PNG
-#define EXPORT_FRAMES 100
+// [uint] Max number of frames exported as PNG.
+/* #define EXPORT_FRAMES 100 */
 #define PROMPT_TO_START false
-// Close the application after this many frames. Useful for profiling. Uncomment to disable.
+// [uint] Close the application after this many frames. Useful for profiling. Uncomment to disable.
 /* #define EXIT_AFTER_FRAMES 3 */
-// Close the application after this many populations. Useful for profiling. Uncomment to disable.
+// [uint] Close the application after this many populations. Useful for profiling. Uncomment to disable.
 /* #define EXIT_AFTER_POPULATIONS 5 */
+
+// [bool] Whether to use the kernel with shared memory.
 #define USE_SHARED_MEMORY true
+// [bool] Whether to use `srand` and `rand` methods for random number sampling (`true`), or a cryptographically secure RNG `arc4random` (`false`).
 #define DETERMINISTIC_RANDOMNESS false
-// Whether to enable `debug_synchronous` for all CUB calls to print debug info.
+// [bool] Whether to enable `debug_synchronous` for all CUB calls to print debug info.
 #define CUB_DEBUG_SYNCHRONOUS false
-// Max number of simultaneously running thread blocks performing crossover, each
+// [uint] Max number of simultaneously running thread blocks performing crossover, each
 // with a separate random number generator.
 #define CROSSOVER_MUTATE_BLOCKS_MAX 16
-// Number of simultaneously running crossover kernels, each with their own set of
+// [uint] Number of simultaneously running crossover kernels, each with their own set of
 // crossover blocks.
 #define CROSSOVER_MUTATE_KERNELS_MAX 32
 
